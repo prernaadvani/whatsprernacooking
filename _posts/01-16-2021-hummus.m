72fc6224498847025acdd673728ec73405c3e6bc
Prerna Advani <43261018+prernaadvani@users.noreply.github.com>
2021-01-17 19:15:54 -0500

    Create 01-16-2021-hummus.m

---
+++ _posts/01-16-2021-hummus.m
@@ -0,0 +1,48 @@
+---
+title: "Hummus"
+categories:
+  - Mediterranean
+  - Sides
+  - Vegan
+image:
+  path: /images/hummus/hummus 2.png
+  thumbnail: /images/hummus/hummus 1.png
+---
+
+I used to go through a box of hummus every week during my freshman year of college, and I finally learned how to make it myself!
+
+The hummus recipe that I used is adapted from [Eating Bird Food](https://www.eatingbirdfood.com/the-key-to-smooth-hummus/).
+The tahini recipe that I used is adapted from [Inspired Taste](https://www.inspiredtaste.net/26901/easy-tahini-recipe/).
+
+### Ingredients:
+
+_Per 4 servings:_
+
+* 1/4 cup tahini:
+  - 1/2 cup sesame seeds
+  - 3 tablespoons olive oil
+  - salt, to taste
+* 1 15-oz can of chickpeas, drained and rinsed1 
+* 3 tablespoons lemon juice
+* 3 Tablespoons olive oil, plus a little extra as a garnish
+* 2 garlic cloves, minced
+* salt, to taste
+* 3 tablespoons water
+
+
+
+
+### Instructions:
+
+* First, we make the tahini! On a medium nonstick pan, add the sesame seeds to toast. Stir continuously until the seeds get darker, about 4-5 minutes. Careful not to burn the seeds - adjust heat accordingly. 
+
+* Transfer the sesame seeds to a plate and let cool completely.
+
+* When the seeds are cooled, transfer them to a food processor and blend for ~1 minute until completely broken down. Add the olive oil and process for a minute or two. Add the salt and process again - the tahini should be smooth at this point.
+
+* To the tahini, add the lemon juice and process. Next, add the rest of the hummus ingredients except the water and process until combined and scraping down the sides as needed. Gradually add the water and continue processing until you have a smooth, creamy hummus. 
+
+![_config.yml]({{ site.baseurl }}/images/hummus/hummus 3.png)
+
+* Transfer to a bowl and serve! I like using it in my falafel bowls (link available [here](https://prernaadvani.github.io/mediterranean/vegan/falafel-bowl/)), with pita (recipe pending), or with carrots.
+
